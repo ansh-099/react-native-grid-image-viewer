e3b6402f64d10a318d7cb9b3a9a0c51ea5fb60e8
Jernej Karba <46779920+jernejkarba@users.noreply.github.com>
2021-03-30 12:08:01 +0200

    Update GridImageViewer.js

--- components/GridImageViewer.js
+++ components/GridImageViewer.js
@@ -106,78 +106,101 @@ const GridImageView = ({ data, headers = null, renderGridImage = null, renderMod
                 contentContainerStyle={{ paddingBottom: 40 }}
                 data={data}
                 renderItem={({ item, index }) => {
-                    if (data.length <= index * 3) {
+                    if (data.length <= index * 4) {
                         return null;
                     }
                     return (
                         <View style={styles.unit}>
                             <View style={styles.unit_item}>
-                                {data.length > index * 3 ?
+                                {data.length > index * 4 ?
                                     <TouchableOpacity
                                         onPress={() => {
-                                            setModal({ visible: true, data: index * 3 });
-
+                                            setModal({ visible: true, data: index * 4 });
                                             setTimeout(() => {
-                                                ref.current.scrollTo({ x: Dimensions.get('window').width * index * 3, y: 0, animated: false });
+                                                ref.current.scrollTo({ x: Dimensions.get('window').width * index * 4, y: 0, animated: false });
                                             }, 1);
 
                                         }}
-                                        style={styles.unit_item}
+                                        style={styles.unit_item_img}
                                     >
                                         {renderGridImage !== null
-                                            ? renderGridImage(data[index * 3], styles.img)
+                                            ? renderGridImage(data[index * 4], styles.img)
                                             : <Image
                                                 style={styles.img} 
                                                 source={{
-                                                    uri: data[index * 3].image,
+                                                    uri: data[index * 4].image,
                                                     ...(headers == null || headers == undefined || headers == {} ? {} : {method: 'POST', headers})
                                                 }} />
                                         }
                                     </TouchableOpacity> : null}
                             </View>
                             <View style={styles.unit_item}>
-                                {data.length > index * 3 + 1 ?
+                                {data.length > index * 4 + 1 ?
                                     <TouchableOpacity
                                         onPress={() => {
-                                            setModal({ visible: true, data: index * 3 + 1 });
+                                            setModal({ visible: true, data: index * 4 + 1 });
 
                                             setTimeout(() => {
-                                                ref.current.scrollTo({ x: Dimensions.get('window').width * (index * 3 + 1), y: 0, animated: false });
+                                                ref.current.scrollTo({ x: Dimensions.get('window').width * (index * 4 + 1), y: 0, animated: false });
                                             }, 1);
 
                                         }}
-                                        style={styles.unit_item}
+                                        style={styles.unit_item_img}
                                     >
                                         {renderGridImage !== null
-                                            ? renderGridImage(data[index * 3 + 1], styles.img)
+                                            ? renderGridImage(data[index * 4 + 1], styles.img)
                                             : <Image
                                                 style={styles.img} 
                                                 source={{
-                                                    uri: data[index * 3 + 1].image,
+                                                    uri: data[index * 4 + 1].image,
                                                     ...(headers == null || headers == undefined || headers == {} ? {} : {method: 'POST', headers})
                                                 }} />
                                         }
                                     </TouchableOpacity> : null}
                             </View>
                             <View style={styles.unit_item}>
-                                {data.length > index * 3 + 2 ?
+                                {data.length > index * 4 + 2 ?
                                     <TouchableOpacity
                                         onPress={() => {
-                                            setModal({ visible: true, data: index * 3 + 2 });
+                                            setModal({ visible: true, data: index * 4 + 2 });
 
                                             setTimeout(() => {
-                                                ref.current.scrollTo({ x: Dimensions.get('window').width * (index * 3 + 2), y: 0, animated: false });
+                                                ref.current.scrollTo({ x: Dimensions.get('window').width * (index * 4 + 2), y: 0, animated: false });
                                             }, 1);
 
                                         }}
-                                        style={styles.unit_item}
+                                        style={styles.unit_item_img}
                                     >
                                         {renderGridImage !== null
-                                            ? renderGridImage(data[index * 3 + 2], styles.img)
+                                            ? renderGridImage(data[index * 4 + 2], styles.img)
                                             : <Image
                                                 style={styles.img} 
                                                 source={{
-                                                    uri: data[index * 3 + 2].image,
+                                                    uri: data[index * 4 + 2].image,
+                                                    ...(headers == null || headers == undefined || headers == {} ? {} : {method: 'POST', headers})
+                                                }} />
+                                        }
+                                    </TouchableOpacity> : null}
+                            </View>
+                            <View style={styles.unit_item}>
+                                {data.length > index * 4 + 3 ?
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            setModal({ visible: true, data: index * 4 + 3 });
+
+                                            setTimeout(() => {
+                                                ref.current.scrollTo({ x: Dimensions.get('window').width * (index * 4 + 3), y: 0, animated: false });
+                                            }, 1);
+
+                                        }}
+                                        style={styles.unit_item_img}
+                                    >
+                                        {renderGridImage !== null
+                                            ? renderGridImage(data[index * 4 + 3], styles.img)
+                                            : <Image
+                                                style={styles.img} 
+                                                source={{
+                                                    uri: data[index * 4 + 3].image,
                                                     ...(headers == null || headers == undefined || headers == {} ? {} : {method: 'POST', headers})
                                                 }} />
                                         }
@@ -200,7 +223,8 @@ const GridImageView = ({ data, headers = null, renderGridImage = null, renderMod
 
 const styles = StyleSheet.create({
     background: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#F9F9F9'
     },
     flatlist: {
         flex: 1 
@@ -209,12 +233,21 @@ const styles = StyleSheet.create({
         flexDirection: 'row'
     },
     unit_item: {
-        height: Dimensions.get('window').height / 5.5,
+        height: 60,
+        width: 60,
         margin: 1.5,
+        flex: 1
+    },
+    unit_item_img: {
+        height: 60,
+        width: 60,
         flex: 1,
+        borderWidth: 1,
+        borderColor: '#3CA082',
+        borderRadius: 1
     },
     img: {
-        flex: 1,
+        flex: 1
     },
     img_modal: {
         height: Dimensions.get('window').height,
